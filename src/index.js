79b3291b48b0163dd7de42bea7e1447061323bc0
Yuvraj Singh <contact.yuvraj08@gmail.com>
2025-04-30 12:52:55 +0530

    make the server listen

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 // require("dotenv").config({path: "./.env",});
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./.env",
-})
+  path: "./.env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  });
 
 // import express from "express";
 
