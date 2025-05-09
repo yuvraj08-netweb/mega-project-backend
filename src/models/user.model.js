d7e9db0fb9611ea21d28b05b4cc831a9d17ef2c5
Yuvraj <yuvraj.netweb@gmail.com>
2025-05-09 17:24:16 +0530

    started models

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar:{
+        type: String, 
+    }
+
+});
+
+export const User = mongoose.model("User", userSchema);
