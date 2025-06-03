a87b2aedad7b1fd1abb9b640e482e2c697b4f902
Yuvraj <yuvraj.netweb@gmail.com>
2025-06-03 16:16:46 +0530

    completed video model and started with user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,31 +1,59 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:{
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar:{
-        type: String, 
-    }
+    avatar: {
+      type: String, // link to the image from cloudinary
+      required: true,
+    },
+    coverImage: {
+      type: String, // link to the image from cloudinary
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+userSchema.pre("save", async function (next) {
+  this.password = bcrypt.hash(this.password, 10);
+  next();
 });
 
 export const User = mongoose.model("User", userSchema);
